d7cdade4848a3a51adbd35bbff9c3bae08b39193
Vincent ROBERT <vincent.robert@cdfnancy.fr>
2021-01-21 10:53:48 +0100

    x est déclaré comme un entier

--- exemple1.cpp
+++ exemple1.cpp
@@ -13,7 +13,7 @@ Fonction principale
 int main()	// Fonction principale
 {
 	SetConsoleOutputCP(1252); // pour les accents (il faut <windows.h>
-	
+	int x,
 	_getch();	// attente d'appui sur une touche
 	return 0;	// fin du programme. Le code 0 est envoyé
 }
