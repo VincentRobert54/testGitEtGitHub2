1cc3ed2ca7c3750fca874f6111d1eb7a1f93d234
Vincent ROBERT <vincent.robert@cdfnancy.fr>
2021-01-21 11:03:33 +0100

    test de la fonction 1

---
+++ fct1.cpp
@@ -0,0 +1,29 @@
+/***************************
+Test de la fonction 1
+*****************************/
+#include <iostream>	// bibliothèque de gestion des E/S
+#include <conio.h>	// gestion de la console (ici _getch())
+#include <windows.h>
+
+using namespace std;
+
+/*****************************
+Fonction 1
+*****************************/
+void fonction1()
+{
+	//... ici se trouvera le code de la fonction 1 que je veux tester
+}
+
+/*****************************
+Fonction principale
+*****************************/
+int main()	// Fonction principale
+{
+	SetConsoleOutputCP(1252);
+
+	fonction1(); // appel de la fonction1 pour vérifier son fonctionnement
+
+	_getch();	// attente d'appui sur une touche
+	return 0;	// fin du programme. Le code 0 est envoyé
+}
